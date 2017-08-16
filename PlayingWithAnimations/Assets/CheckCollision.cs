6e28fa9642046c8852962a2095180fb4ac11d3cc
mancu37 <mancu37@gmail.com>
2017-08-16 16:04:39 -0300

    Simple Soccer Penalty Game
    Under development...

---
+++ PlayingWithAnimations/Assets/CheckCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CheckCollision : MonoBehaviour {
+
+    public Transform pelota;
+    public Transform direccion;
+
+    public float fuerza;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.name.Contains("Plane"))
+        {
+            Debug.LogFormat("Collider: {0}", other.name);
+
+            //pelota.GetComponent<Rigidbody>().AddForce((pelota.position - transform.position) * fuerza);
+
+            pelota.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.forward) * fuerza);
+        }
+    }
+
+    private void Update()
+    {
+        Ray ray = new Ray(transform.position + Vector3.up, -Vector3.up);
+        RaycastHit hitInfo = new RaycastHit();
+
+        Debug.DrawRay(transform.position, pelota.position - transform.position, Color.red);
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            pelota.GetComponent<Rigidbody>().AddForce((transform.position - pelota.position) * fuerza);
+        }
+    }
+
+
+}
